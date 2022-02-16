b15f60de156cf3e98bcd389667757765c5d6015c
esmanurAydogan <99769831+esmanurAydogan@users.noreply.github.com>
2022-02-16 04:24:50 +0300

    Add files via upload

---
+++ replaceTheValues.py
@@ -0,0 +1,15 @@
+int1 = 1
+int2 = 2
+
+print("int1 : " ,int1)
+print("int2 : ", int2)
+
+
+int1 = int1 + int2 # int1 = 3, int2 = 2
+int2 = int1 - int2 # int2 = 1 ,int1 = 3
+int1 = int1 - int2 # int1 = 2 int2 = 1
+print("Values were replaced.\t New values : ")
+print("int1 : ", int1)
+print("int2 : " ,int2)
+
+
